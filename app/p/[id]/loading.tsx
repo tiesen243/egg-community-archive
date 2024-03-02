3e6db6587e81e551cb1dd4ea7a039257c491de84
tiesen243 <ttien56906@gmail.com>
2024-03-03 01:07:28 +0700

    dasssa

--- app/p/[id]/loading.tsx
+++ app/p/[id]/loading.tsx
@@ -1,10 +1,9 @@
-import { Separator } from '@/components/ui/separator'
 import { Skeleton } from '@/components/ui/skeleton'
 import type { NextPage } from 'next'
 
 const Loading: NextPage = () => {
   return (
-    <main className="container max-w-screen-md flex-grow">
+    <main className="container max-w-screen-md flex-grow space-y-4">
       <div className="flex items-center gap-2">
         <Skeleton className="size-10 rounded-full" />
         <div className="space-y-2">
@@ -12,8 +11,6 @@ const Loading: NextPage = () => {
           <Skeleton className="h-4 w-24" />
         </div>
       </div>
-
-      <Separator className="my-4" />
 
       <Skeleton className="h-44 w-full" />
     </main>
