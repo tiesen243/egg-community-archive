4740458562a758c4220cec421b181ea9e4be4381
tiesen243 <ttien56906@gmail.com>
2024-03-03 01:35:33 +0700

    fix middleware error

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,10 @@
-import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { auth } from './server/auth'
+import { NextResponse } from 'next/server'
 
 export async function middleware(request: NextRequest) {
-  const session = await auth()
-  if (!session) {
-    return NextResponse.redirect(new URL('/auth/signin', request.url))
-  }
+  const cookies = request.cookies
+  const sessionToken = cookies.get('authjs.session-token')
+  if (!sessionToken) return NextResponse.redirect(new URL('/auth/signin', request.url))
 }
 
 export const config = {
