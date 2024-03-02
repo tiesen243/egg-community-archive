d5d158865f8e2b8af04a969799426a4e27956daf
tiesen243 <ttien56906@gmail.com>
2024-03-03 01:40:23 +0700

    dasda

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,11 @@ import { NextResponse } from 'next/server'
 
 export async function middleware(request: NextRequest) {
   const cookies = request.cookies
-  const sessionToken = cookies.get('authjs.session-token')
+  const sessionToken =
+    process.env.NODE_ENV === 'development'
+      ? cookies.get('authjs.session-token')
+      : cookies.get('__Secure-authjs.session-token')
+
   if (!sessionToken) return NextResponse.redirect(new URL('/auth/signin', request.url))
 }
 
